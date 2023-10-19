83d10515eb75bc6f46f4206f670ff01b7e1b5089
Adilene Jesus da Silva <jesusadilene121@gmail.com>
2023-10-18 23:33:48 -0300

    commit inicial...

---
+++ android/app/src/main/kotlin/com/adilene/app/meuappflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adilene.app.meuappflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ meuapp/android/app/src/main/kotlin/com/adilene/app/meuapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adilene.app.meuapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
